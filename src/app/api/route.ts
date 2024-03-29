073d690b5e3c972ba0eca3b502f48cc939ac942c
Naeema bargir <bargirn5@gmail.com>
2024-03-29 14:02:21 +0530

    api changes

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,19 +1,10 @@
 // import connectDB from '../../utils/db';
-import { NextApiRequest, NextApiResponse } from 'next';
 import User from '../utilis/models';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === 'POST') {
-        const { name, email , phone } = req.body;
-        console.log(name, email)
-        try {
-            res.status(201).json({ success: true, message: 'User created successfully' });
-        } catch (error) {
-            console.error('Error saving user data:', error);
-            res.status(500).json({ success: false, message: 'Failed to create user' });
-        }
-    } else {
-        res.status(405).json({ success: false, message: 'Method Not Allowed' });
-    }
+export async function POST(request: Request) {
+    console.log(request)
+    return new Response('Success!', {
+        status: 200,
+    })
 }
 
