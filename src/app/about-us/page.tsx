4e3145a61932710e62ce38e7ade625c8f63efad8
Naeema bargir <bargirn5@gmail.com>
2024-03-29 12:35:02 +0530

    img tag remove

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -9,14 +9,14 @@ const About = () => {
             <Breadcrumb paths={[{ title: 'About', url: '/about-us', disabled: true }]} title="About Us" />
             <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
                 <div className="grid grid-cols-2 gap-4 mt-8">
-                    <img
+                    {/* <img
                         className="mt-4 w-full lg:mt-10 rounded-lg"
                         src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-1.png"
                         alt="office content 2" />
                     <img
                         className="w-full rounded-lg"
                         src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png"
-                        alt="office content 1" />
+                        alt="office content 1" /> */}
                 </div>
                 <div className="font-light sm:text-lg">
                     <h2 className="title ">One Stop For All Your Car Solutions</h2>
