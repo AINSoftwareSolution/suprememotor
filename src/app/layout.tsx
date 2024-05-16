4fa5b852323f375dd771fbb25e934b31e2a7fb52
Naeema bargir <bargirn5@gmail.com>
2024-05-16 14:56:17 +0530

    meta description updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
   keywords:
     "car mechanic near me, car repair near me, car mechanic in pune, car repair services in pune, auto shop near me, mechanic shop near me, service, car, paint, jobs, interior, insurance, body shop, custom paint, book appointment, expertise meet, paint jobs, car repair shop near me, cashless insurance",
   description:
-    "Welcome to Supreme Motor Company, your ultimate destination for car restoration and custom paint jobs in Pune, India. We offer Ceranic and Graphene coating, Paint Protection Films, Body-shop(Denting and painting), Detaking and Interior Cleaning, Wrap jobs and Interior Modification.",
+    "Welcome to Supreme Motor Company, your ultimate destination for car repair, mechanic services and  custom paint jobs in Pune, India. We offer best automotive services like Ceranic and Graphene coating, Paint Protection Films, Body-shop(Denting and painting), Detaking and Interior Cleaning, Wrap jobs and Interior Modification. We provide Cashless Insurance",
 };
 
 export default function RootLayout({
